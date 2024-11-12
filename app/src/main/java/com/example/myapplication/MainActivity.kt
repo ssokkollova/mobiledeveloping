572a6a699698a2f44a892ffd096157edabadc7bf
Татьяна <ssokkolllovva@yandex.ru>
2024-11-13 00:17:46 +0300

    Прогноз погоды 9. Полное интегрирование API в карточки

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -36,7 +36,20 @@ class MainActivity : ComponentActivity() {
                 val daysList = remember {
                     mutableStateOf(listOf<WeatherModel>())
                 }
-                getData("London", this, daysList)
+                val currentDay = remember {
+                    mutableStateOf(WeatherModel(
+                        "",
+                        "",
+                        "0.0",
+                        "",
+                        "",
+                        "0.0",
+                        "0.0",
+                        "",
+                    )
+                    )
+                }
+                getData("London", this, daysList, currentDay)
                 Image(
                     painter = painterResource(id = R.drawable.background_im),
                     contentDescription = "im1",
@@ -46,7 +59,7 @@ class MainActivity : ComponentActivity() {
                     contentScale = ContentScale.FillHeight
                 )
                 Column {
-                    MainCard()
+                    MainCard(currentDay)
                     TabLayout(daysList)
                 }
             }
@@ -56,7 +69,9 @@ class MainActivity : ComponentActivity() {
 
 
 
-private fun getData(city: String, context: Context, daysList: MutableState<List<WeatherModel>>){
+private fun getData(city: String, context: Context,
+                    daysList: MutableState<List<WeatherModel>>,
+                    currentDay: MutableState<WeatherModel>){
     val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
             "&q=$city"+
             "&days=" +
@@ -68,6 +83,7 @@ private fun getData(city: String, context: Context, daysList: MutableState<List<
             url,
             { response ->
                 val list = getWeatherByDays(response)
+                currentDay.value = list[0]
                 daysList.value = list
             },
             { Log.d("MyLog", "VolleyError $it")
--- app/src/main/java/com/example/myapplication/screens/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/MainScreen.kt
@@ -40,9 +40,9 @@ import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.MutableState
 
-@Preview(showBackground = true)
+
 @Composable
-fun MainCard() {
+fun MainCard(currentDay: MutableState<WeatherModel>) {
     Column(
         modifier = Modifier
             .padding(5.dp),
@@ -71,12 +71,12 @@ fun MainCard() {
                     ) {
                         Text(
                             modifier = Modifier.padding(top = 8.dp),
-                            text = "20 Jun 2022 13:00",
+                            text = currentDay.value.time,
                             style = TextStyle(fontSize = 15.sp),
                             color = Color.White
                         )
                         AsyncImage(
-                            model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                            model = " + currentDay.value.icon",
                             contentDescription = "im2",
                             modifier = Modifier
                                 .padding(
@@ -87,17 +87,17 @@ fun MainCard() {
                         )
                     }
                     Text(
-                        text = "Madrid",
+                        text = currentDay.value.city,
                         style = TextStyle(fontSize = 25.sp),
                         color = Color.White
                     )
                     Text(
-                        text = "23°C",
+                        text = currentDay.value.currentTemp.toFloat().toInt().toString() + "°C",
                         style = TextStyle(fontSize = 65.sp),
                         color = Color.White
                     )
                     Text(
-                        text = "Sunny",
+                        text = currentDay.value.condition,
                         style = TextStyle(fontSize = 16.sp),
                         color = Color.White
                     )
@@ -116,7 +116,9 @@ fun MainCard() {
                             )
                         }
                         Text(
-                            text = "23°C/12°C",
+                            text = "${currentDay.value
+                                .maxTemp.toFloat().toInt()}°C/${currentDay
+                                .value.minTemp.toFloat().toInt()}°C",
                             style = TextStyle(fontSize = 16.sp),
                             color = Color.White
                         )
