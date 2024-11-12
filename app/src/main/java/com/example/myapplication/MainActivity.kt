794c060f9ef2308428942f7215c1ef0b04c1d391
Татьяна <ssokkolllovva@yandex.ru>
2024-11-13 00:09:40 +0300

    Прогноз погоды 7. getData

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,32 +7,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.myapplication.screens.MainCard
 import com.example.myapplication.screens.TabLayout
-
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import org.json.JSONObject
 
 const val API_KEY = "7f6eebb6101546f29f2122941242210"
 
@@ -42,6 +28,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
+                getData("London", this)
                 Image(
                     painter = painterResource(id = R.drawable.background_im),
                     contentDescription = "im1",
@@ -59,53 +46,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, context: Context) {
-    val state = remember {
-        mutableStateOf("Unknown")
-    }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier.fillMaxHeight(0.5f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Temp in $name = ${state.value} °C")
-        }
-        Box(
-            modifier = Modifier.fillMaxHeight()
-                .fillMaxWidth(),
-            contentAlignment = Alignment.BottomCenter
-        ) {
-            Button(
-                onClick = {
-                    getResult(name, state, context)
-                }, modifier = Modifier.padding(5.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(text = "Refresh")
-            }
-        }
-    }
-}
 
-    private fun getResult(city: String, state: MutableState<String>, context: Context) {
-        val url = "https://api.weatherapi.com/v1/current.json" +
-                "?key=$API_KEY&" +
-                "q=$city" +
-                "&aqi=no"
+
+private fun getData(city: String, context: Context){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city"+
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no\n"
         val queue = Volley.newRequestQueue(context)
-        val stringRequest = StringRequest(
+        val sRequest = StringRequest(
             Request.Method.GET,
             url,
             { response ->
-                val obj = JSONObject(response)
-                state.value = obj.getJSONObject("current").getString("temp_c")
+                Log.d("MyLog", "Response : $response")
             },
-            { error ->
-                Log.d("MyLog", "Error $error")
+            { Log.d("MyLog", "VolleyError $it")
             }
         )
-        queue.add(stringRequest)
+        queue.add(sRequest)
     }
 
