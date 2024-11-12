bc9d886459634ce42283bf962b637ba00938e651
Татьяна <ssokkolllovva@yandex.ru>
2024-11-12 23:43:14 +0300

    Прогноз погоды 3. сделали первую карточку

--- app/src/main/java/com/example/myapplication/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/MainScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -55,21 +57,76 @@ fun MainScreen() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row (modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ){
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(top = 8.dp),
+                            text = "20 Jun 2022 13:00",
+                            style = TextStyle(fontSize = 15.sp),
+                            color = Color.White
+                        )
+                        AsyncImage(
+                            model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                            contentDescription = "im2",
+                            modifier = Modifier
+                                .padding(
+                                    top = 3.dp,
+                                    end = 8.dp
+                                )
+                                .size(35.dp)
+                        )
+                    }
                     Text(
-                        modifier = Modifier.padding(top = 8.dp),
-                        text = "20 Jun 2022 13:00",
-                        style = TextStyle(fontSize = 15.sp),
+                        text = "Madrid",
+                        style = TextStyle(fontSize = 25.sp),
                         color = Color.White
                     )
-                    AsyncImage(
-                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
-                        contentDescription = "im2",
-                        modifier = Modifier.size(35.dp)
-                            .padding(top = 3.dp, end = 8.dp)
+                    Text(
+                        text = "23°C",
+                        style = TextStyle(fontSize = 65.sp),
+                        color = Color.White
                     )
+                    Text(
+                        text = "Sunny",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(
+                            onClick = {
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_search),
+                                contentDescription = "im3",
+                                tint = Color.White
+                            )
+                        }
+                        Text(
+                            text = "23°C/12°C",
+                            style = TextStyle(fontSize = 16.sp),
+                            color = Color.White
+                        )
+                        IconButton(
+                            onClick = {
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_sync),
+                                contentDescription = "im4",
+                                tint = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
