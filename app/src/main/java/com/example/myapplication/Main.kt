39f69a910fb5349af94d7a08a5087f3f84d1ca0a
Татьяна <ssokkolllovva@yandex.ru>
2024-11-12 21:11:47 +0300

    Задание 1 вариант 26

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,5 +1,26 @@
 package com.example.myapplication
 
 fun Main() {
+    print("Введите количество килограммов конфет: ")
+    val kg = readLine()!!.toDouble()
 
+    print("Введите стоимость этих конфет в рублях: ")
+    val price = readLine()!!.toDouble()
+
+    print("Введите количество килограммов конфет, для которых нужно определить стоимость: ")
+    val kgToPrice = readLine()!!.toDouble()
+
+    print("Введите сумму денег, на которую нужно купить конфеты: ")
+    val money = readLine()!!.toDouble()
+
+
+    val pricePerKg = price / kg
+    val totalPrice = pricePerKg * kgToPrice
+
+
+    val kgToBuy = money / pricePerKg
+
+
+    println("Стоимость $kgToPrice кг конфет: $totalPrice рублей")
+    println("На $money рублей можно купить $kgToBuy кг конфет")
 }
