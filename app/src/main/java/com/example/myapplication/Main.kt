45d150197bd76bee799fcae91c822b7666dd6789
Татьяна <ssokkolllovva@yandex.ru>
2024-11-12 23:33:29 +0300

    Прогноз погоды 1. библиотека Volley и Api weather

--- app/src/main/java/com/example/myapplication/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication
-
-fun Main() {
-
-}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import org.json.JSONObject
+
+const val API_KEY = "7f6eebb6101546f29f2122941242210"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +39,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    Greeting("Madrid", this)
                 }
             }
         }
@@ -31,17 +52,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+fun Greeting(name: String, context: Context) {
+    val state = remember {
+        mutableStateOf("Unknown")
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier.fillMaxHeight(0.5f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Temp in $name = ${state.value} °C")
+        }
+        Box(
+            modifier = Modifier.fillMaxHeight()
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(
+                onClick = {
+                    getResult(name, state, context)
+                }, modifier = Modifier.padding(5.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Refresh")
+            }
+        }
     }
 }
+
+    private fun getResult(city: String, state: MutableState<String>, context: Context) {
+        val url = "https://api.weatherapi.com/v1/current.json" +
+                "?key=$API_KEY&" +
+                "q=$city" +
+                "&aqi=no"
+        val queue = Volley.newRequestQueue(context)
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            { response ->
+                val obj = JSONObject(response)
+                state.value = obj.getJSONObject("current").getString("temp_c")
+            },
+            { error ->
+                Log.d("MyLog", "Error $error")
+            }
+        )
+        queue.add(stringRequest)
+    }
+
